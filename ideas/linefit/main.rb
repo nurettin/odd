a0a5b027d4aaf4911e47a641bc09ca268e0f1b97
Nurettin Onur TUGCU <onur.tugcu@gmail.com>
2014-03-10 03:29:07 +0200

    draw linear regression

--- ideas/linefit/main.rb
+++ ideas/linefit/main.rb
@@ -6,28 +6,36 @@ lf= LineFit.new
 
 x= []
 y= []
-@ts= []
+labels= {}
 CSV.foreach("../data/BITSTAMPUSD_reverse.csv") do |row|
   x<< Date.parse(row[0]).to_time.to_i
   y<< row[1].to_f
-  @ts<< [x, y]
+  labels[x[-1]]= row[0]
 end
 
-lf.setData(x, y)
+len_err= Hash[(2..10).map do |l|
+  [l, (600..810- l).map do |n|
+    xc= x[n..n+ l]
+    yc= y[n..n+ l]
+    lf.setData(xc, yc)
+    lf.residuals.map(&:abs).max
+  end]
+end]
 
-g = Gruff::Line.new(400)
-g.title = 'Many Values Line Test 400px'
-g.labels = {
-    0 => '5/6',
-    10 => '5/15',
-    20 => '5/24',
-    30 => '5/30',
-    40 => '6/4',
-    50 => '6/16'
-}
-%w{jimmy jane philip arthur julie bert}.each do |student_name|
-  g.data(student_name, (0..50).collect { |i| rand 100 })
+g = Gruff::Line.new(800)
+g.title = "btstamp"
+len_err.each do |k, v|
+  g.data(k, v)
 end
+g.write("draw.png")
 
-# Default theme
-g.write('line_many_lines_small.png')
+=begin
+g = Gruff::Line.new(800)
+g.title = "btstamp"
+g.labels= labels
+g.data("open", yc)
+g.data("fit", lf.predictedYs)
+g.data("errmax", (1..l).map{ max })
+g.data("err", lf.residuals)
+g.write('draw.png')
+=end
